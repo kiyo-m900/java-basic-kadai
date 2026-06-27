c10ea5e561cfa1f9e7b56e10533e6afe23e884db
kiyo-m900 <lpp.palop.68@gmail.com>
2026-06-27 20:30:29 +0900

    21章課題の提出

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,16 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		//インスタンス化
+		Dictionary_Chapter21 dic = new Dictionary_Chapter21();
+		
+		//辞書の作成
+		dic.addFruit();
+		
+		//単語の検索
+		dic.checkFruit("apple");
+	}
+
+}
---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,34 @@
+package kadai_021;
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+	//HashMapの宣言
+	HashMap<String,String> fruitMap = new HashMap<String,String>();
+	
+	//追加
+	public void addFruit() {
+		fruitMap.put("apple", "りんご");
+		fruitMap.put("peach", "桃");
+		fruitMap.put("banana", "バナナ");
+		fruitMap.put("lemon", "レモン");
+		fruitMap.put("pear", "梨");
+		fruitMap.put("kiwi", "キウィ");
+		fruitMap.put("strawberry", "いちご");
+		fruitMap.put("grape", "ぶどう");
+		fruitMap.put("muscat", "マスカット");
+		fruitMap.put("cherry", "さくらんぼ");
+	}
+	
+	//繰り返し処理による単語検索
+	public void checkFruit(String word) {
+		for(int i = 0; i <= fruitMap.size(); i++) {
+			if(fruitMap.containsKey(word)) {
+				System.out.println(word + "の意味は" + fruitMap.get(word));
+				break;
+			} else {
+				System.out.println(word + "は辞書に存在しません");
+				break;
+			}
+		}
+	}
+}
